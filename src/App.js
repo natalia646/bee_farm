55f94a6085208d0abc70085521e6e2ff2e92ba52
natalia646 <nataxash46@gmail.com>
2023-06-15 13:34:47 +0300

    added component WhatWeDo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Heder from "./components/Heder/Heder";
 import Title from "./components/Title/Title";
+import WhatWiDo from "./components/WhatWiDo/WhatWiDo";
 
 function App() {
   return (
-    <div className='fon '>
-      <Heder/>
-      <Title/>
+    <div className="page">
+      <div className="fon ">
+        <Heder />
+        <Title />
+      </div>
+      <div className="products">
+        <WhatWiDo/>
+      </div>
     </div>
   );
 }
---
+++ src/components/WhatWiDo/WhatWiDo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './WhatWiDo.css';
+// import icon1 from '/img/icon/icon1.svg';
+
+
+export default function WhatWiDo() {
+
+
+  return (
+    <div>
+      <h2>WhatWiDo</h2>
+      <p>Наша сімейна компанія займається всім, що стосується меду та його виробництва. Дізнайтеся більше про наші послуги нижче.</p>
+      <div className='blocks'>
+        <article className='article'>
+            {/* <img src= {icon1} ></img> */}
+
+      </article>
+      </div>
+      
+    </div>
+  )
+}
